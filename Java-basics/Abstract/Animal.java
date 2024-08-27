a00063815d76c5e65087dca96cbd76672bc7d82b
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2024-08-27 18:42:29 +0300

    Java-Basics

---
+++ Java-basics/Abstract/Animal.java
@@ -0,0 +1,10 @@
+package Abstract;
+
+public abstract class Animal {
+
+    public abstract void makeSound();
+
+    public void sleep(){
+        System.out.println("ZzZz...");
+    }
+}
---
+++ Java-basics/Abstract/Main.java
@@ -0,0 +1,9 @@
+package Abstract;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.makeSound();
+        myDog.sleep();
+    }
+}
